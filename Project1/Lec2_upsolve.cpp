3f3cd5e9dbbaf7f36eda795f7b83aa2542846a2e
muhammedkhaled0 <muhammedkhaled7882@gmail.com>
2026-03-01 17:33:03 +0200

    adding problem 2

--- Project1/Lec2_upsolve.cpp
+++ Project1/Lec2_upsolve.cpp
@@ -1,3 +1,6 @@
+
+//=============================================================problem 1==================================================
+/*
 #include<iostream>
 #include<stack>
 #include<string>
@@ -29,3 +32,54 @@ string editingStr(string s) {
     }
     return returnedStr;
 }
+*/
+//=============================================================problem 2==================================================
+/*
+#include<iostream>
+#include<stack>
+using namespace std;
+int main() {
+	cout << "Enter The String : ";
+	string input;
+	cin >> input;
+	stack<int> st;
+	for (int i = 0; i < input.size(); i++) {
+		switch (input[i]) {
+		case 'W':
+		case 'w':
+			st.push(5);
+			break;
+		case 'D':
+		case 'd':
+			st.push(10);
+			break;
+		case 'C':
+		case 'c':
+			if (!st.empty()) st.pop();
+			break;
+		case 'S':
+		case 's':
+			int last1 = 0, last2 = 0;
+			if (st.size() >= 2) {
+				int last1 = st.top(); st.pop();
+				int last2 = st.top(); st.pop();
+				st.push(last2);
+				st.push(last1);
+				st.push(last1 + last2);
+			}
+			else if (st.size() == 1) {
+				int last1 = st.top(); st.pop();
+				st.push(last1);
+				st.push(last1);
+				break;
+			}
+		}
+	}
+		int sum = 0;
+		while (!st.empty()) {
+			sum += st.top();
+			st.pop();
+		}
+		cout << sum << '\n';
+}
+*/
